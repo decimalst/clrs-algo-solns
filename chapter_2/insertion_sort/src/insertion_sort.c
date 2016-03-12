71a9bf76fba4f609266192ce9cf75d4be1d72117
Byron Lambrou <byronl@udel.edu>
2016-03-11 19:31:30 -0500

    Added some small details

--- chapter_2/insertion_sort/src/insertion_sort.c
+++ chapter_2/insertion_sort/src/insertion_sort.c
@@ -5,6 +5,18 @@ Takes integer input from STDIN, outputs result to STDOUT
 */
 #include <stdio.h>
 
-int main(){
+int main(int argc, char *argv[]){
+  int key;
+  // Dynamically allocate array to hold sorting
+  int *array_to_sort;
+  array_to_sort = (int *)malloc(sizeof(int)*argc);
+  // Now read over integers using atoi()
+  for (int i=0; i<argc; i++){
+    array_to_sort[i] = atoi(argv[i]);
+    printf(array_to_sort[i]);
+  }
+  for(int j=1; j<argc; j++){
+    key = array_to_story[j]
+  }
 
 }

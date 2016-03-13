36274ec13d54a1b82661a5a765afb4dc17fece50
decimalst <byron.lambrou@gmail.com>
2016-03-13 02:01:59 +0000

    Added framework for c reading from STDIN

---
+++ chapter_2/insertion_sort/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# make clean, recompiles, and automatically runs tests located in '.test/' directory
+# and reports the test results.
--- chapter_2/insertion_sort/src/insertion_sort.c
+++ chapter_2/insertion_sort/src/insertion_sort.c
@@ -4,19 +4,34 @@ Description:Implementation of insertion sort in C
 Takes integer input from STDIN, outputs result to STDOUT
 */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]){
   int key;
-  // Dynamically allocate array to hold sorting
   int *array_to_sort;
-  array_to_sort = (int *)malloc(sizeof(int)*argc);
-  // Now read over integers using atoi()
-  for (int i=0; i<argc; i++){
-    array_to_sort[i] = atoi(argv[i]);
-    printf(array_to_sort[i]);
+  int elements;
+  char input_size [10];
+  char input [1024];
+  char * split;
+  // Get number of variables to sort
+  fgets(input_size, 9, stdin);
+  elements = atoi(input_size);
+  // printf("%d \n",elements);
+  // Dynamically allocate array to hold array
+  array_to_sort = (int *)malloc(sizeof(int)*elements);
+  // Read elements in array from stdin
+  fgets(input, 1024, stdin);
+  // Split string on whitespace
+  split = strtok(input, " ");
+  // Now read it into our array
+  for(int i=0; i<elements; i++){
+    // printf("i = %d \n",i);
+    // printf("num = %d \n",atoi(split));
+    array_to_sort[i]=atoi(split);
+    split = strtok(NULL, " ");
   }
-  for(int j=1; j<argc; j++){
-    key = array_to_story[j]
-  }
-
+  
+  // Clean up to avoid memory leaks
+  free(array_to_sort);
 }
